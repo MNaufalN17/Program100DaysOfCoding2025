ba05620c1d10f144f25f9c22a3e21ca60ac27e3a
Muhammad Naufal. N <146636243+MNaufalN17@users.noreply.github.com>
2025-09-09 16:33:46 +0800

    Add files via upload

---
+++ Days_002.java
@@ -0,0 +1,18 @@
+package Package;
+
+import java.util.Scanner;
+
+public class Days_002 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Input Char");
+    System.out.print("Nama :");
+    System.out.println("Muhammad Naufal. N");
+    String n = "D0223325";
+    short u = 20;
+    double bb = 58.8;
+    boolean m = false;
+    char jk = 'L';
+    System.out.printf("NIM : %s\nBerat Badan : %f\nUMUR : %d\nStatus Pernikahan : %b\nGender : %c",n,bb,u,m,jk);
+}
+}
