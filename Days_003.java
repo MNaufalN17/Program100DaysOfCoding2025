57c7b308f79d1a8d51b98b72ed69b51f2df82f81
Muhammad Naufal. N <146636243+MNaufalN17@users.noreply.github.com>
2025-09-10 17:35:56 +0800

    Add files via upload
    Memahami dan menggunakan Komentar.

---
+++ Days_003.java
@@ -0,0 +1,13 @@
+package Package;
+
+public class Days_003 {
+    public static void main(String[] args) {
+        /*Ini adalah komentar yang dimana ini tidak akan dieksekusi oleh program */
+        String nama = "Muhammad Naufal. N";
+        byte umur = 20;
+        float bb = 58.2f;
+        System.out.printf("Nama :%s",nama+"\n");
+        System.out.printf("Umur :%d",umur);
+        // System.out.printf("BB :%f",bb);
+    }
+}
