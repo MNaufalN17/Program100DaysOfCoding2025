31c41edd02d67d2c663938e04f60ff6f71397fa8
Muhammad Naufal. N <146636243+MNaufalN17@users.noreply.github.com>
2025-09-11 22:23:56 +0800

    Add files via upload
    Penggunaan Escape Sequence (\n, \t, dan lain lain).

---
+++ Days_004.java
@@ -0,0 +1,10 @@
+package Package;
+
+public class Days_004 {
+public static void main(String[] args) {
+    String nama = "Muhammad Naufal. N";
+    System.out.println("Nama"+nama+"\n");
+    System.out.println("Saya Adalah Mahasiswa Dari Unsulbar\"");
+    System.out.println("\t NIM :D0223325");
+}
+}
