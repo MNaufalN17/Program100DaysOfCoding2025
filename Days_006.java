1e4efa70a44680d1e53f195c26e99a8226da8093
Muhammad Naufal. N <146636243+MNaufalN17@users.noreply.github.com>
2025-09-13 21:39:00 +0800

    Add files via upload
    Tipe Data Pecahan (double, float).

---
+++ Days_006.java
@@ -0,0 +1,26 @@
+package Package;
+
+public class Days_006 {
+
+    public static void main(String[] args) {
+        float datafloat = 1.1223f;
+        double datadouble = 312321.234234;
+
+        float floatmin = Float.MIN_VALUE;
+        float floatmax = Float.MAX_VALUE;
+
+        double doublemin = Double.MIN_NORMAL;
+        double doublemax = Double.MAX_VALUE;
+
+        System.out.println("Data Float : "+datafloat);
+        System.out.println("Data Double :"+datadouble);
+        System.out.println("FLoat MIN :"+floatmin);
+        System.out.println("Float MAX :"+floatmax);
+        System.out.println("Double MIN :"+doublemin);
+        System.out.println("Double MAX :"+doublemax);
+
+
+
+    }
+
+}
