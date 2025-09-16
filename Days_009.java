5da8f66ed53a211bccdf7d889f8882e60e304a43
Muhammad Naufal. N <146636243+MNaufalN17@users.noreply.github.com>
2025-09-16 22:22:05 +0800

    Deklarasi, Inisialisasi dan Update Variabel.

---
+++ Days_009.java
@@ -0,0 +1,13 @@
+package Package;
+
+public class Days_009 {
+public static void main(String[] args) {
+    int a =5;
+    int b =10;
+    int c =a;
+     a = b;
+     b = c;
+     System.out.println("a:"+a);
+     System.out.println("b:"+b);
+}
+}
