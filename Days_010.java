899723a40e26c92961dc9a8b4bad1686f3b97be0
Muhammad Naufal. N <146636243+MNaufalN17@users.noreply.github.com>
2025-09-17 23:15:38 +0800

    Final variable

---
+++ Days_010.java
@@ -0,0 +1,12 @@
+package Package;
+
+public class Days_010 {
+public static void main(String[] args) {
+    final String nama = "NNaufalM17";
+    final int umur =20;
+    System.out.println(nama);
+    System.out.println(umur);
+
+
+}
+}
