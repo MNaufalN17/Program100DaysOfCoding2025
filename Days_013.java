a340d538785a0e888dab50a4a5269a695b492065
Muhammad Naufal. N <146636243+MNaufalN17@users.noreply.github.com>
2025-09-21 00:32:24 +0800

    Hari Ke-13 Evaluasi

---
+++ Days_013.java
@@ -0,0 +1,14 @@
+package Package;
+
+public class Days_013 {
+public static void main(String[] args) {
+    double warisan = 120_000;
+    double potongan = warisan*0.25;
+    System.out.println(potongan);
+    double ptn = warisan-potongan;
+    System.out.println("Hasil Bagi : "+ptn);
+    double hasilbagi = ptn/3;
+
+    System.out.println(hasilbagi);
+}
+}
