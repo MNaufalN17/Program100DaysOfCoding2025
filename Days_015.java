10767b0b1a97b293e5e7de8aa73030a7ed017813
Muhammad Naufal. N <146636243+MNaufalN17@users.noreply.github.com>
2025-09-22 21:33:22 +0800

    Operator Aritmatika Penjumlahan dan Pengurangan
    Operator Aritmatika Penjumlahan dan Pengurangan

---
+++ Days_015.java
@@ -0,0 +1,11 @@
+package Package;
+
+public class Days_015 {
+public static void main(String[] args) {
+    int a = 10;
+    int b = 7;
+    int c = a+b;
+
+    System.out.println("a + b :"+c);
+}
+}
