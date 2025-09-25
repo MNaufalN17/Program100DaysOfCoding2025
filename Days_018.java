e1def93f0aa0f682b569cc255f00ec0fb176824e
Muhammad Naufal. N <146636243+MNaufalN17@users.noreply.github.com>
2025-09-25 19:51:54 +0800

    Add files via upload
    Konversi Otomatis (Dari Tipe Data Kecil ke Besar).

---
+++ Days_018.java
@@ -0,0 +1,14 @@
+package Package;
+
+public class Days_018 {
+public static void main(String[] args) {
+    int dataint = 1231;
+    long datalong = dataint;
+    float datafloat = datalong;
+    double datadouble = datafloat;
+    System.out.println(dataint);
+    System.out.println(datalong);
+    System.out.println(datafloat);
+    System.out.println(datadouble);
+}
+}
