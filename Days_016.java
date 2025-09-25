7942bff71070b4a3ee192b509bb06ec4dbdeeee2
Muhammad Naufal. N <146636243+MNaufalN17@users.noreply.github.com>
2025-09-25 19:50:38 +0800

    Add files via upload
    Operator Aritmatika Perkalian (*) dan Pembagian (/).

---
+++ Days_016.java
@@ -0,0 +1,12 @@
+package Package;
+
+public class Days_016 {
+public static void main(String[] args) {
+    int a = 10;
+    int b = 5;
+    int c = a/b;
+    int d = a*b;
+    System.out.println("a / b :" +c);
+    System.out.println("a * b :" +d);
+}
+}
