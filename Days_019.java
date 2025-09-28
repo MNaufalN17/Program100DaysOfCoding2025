3441fa1de561a7c01780070b9d551ce2f35088e2
Muhammad Naufal. N <146636243+MNaufalN17@users.noreply.github.com>
2025-09-28 23:48:45 +0800

    Konversi Manual Data Besar ke Kecil
    Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil).

---
+++ Days_019.java
@@ -0,0 +1,11 @@
+
+package Package;
+
+public class Days_019 {
+public static void main(String[] args) {
+    int dataInt = 9999999;
+    Short dataShort = (short) dataInt;
+    System.out.println(dataShort);
+    
+}
+}
