1e663fac53f777654b23ff6efc4537a145df2efd
Muhammad Naufal. N <146636243+MNaufalN17@users.noreply.github.com>
2025-09-28 23:49:52 +0800

    Latihan: Program untuk menukar nilai dua variabel.
    Latihan: Program untuk menukar nilai dua variabel.

---
+++ Days_021.java
@@ -0,0 +1,20 @@
+package Package;
+
+public class Days_021 {
+public static void main(String[] args) {
+    int a = 5;
+    int b = 10;
+    System.out.println("------------------");
+    System.out.println("a :"+a);
+    System.out.println("b :"+b);
+
+    a = a+b; //15
+    b = a-b; //5
+    a = a-b; //10
+
+    System.out.println("------------------");
+    System.out.println("a :"+a);
+    System.out.println("b :"+b);    
+
+}
+}
