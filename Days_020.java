0f587dba2e8accc58d9c03cad410803e7eb0db91
Muhammad Naufal. N <146636243+MNaufalN17@users.noreply.github.com>
2025-09-28 23:49:24 +0800

    Mengubah Tipe Data Primitif menjadi String.
    Mengubah Tipe Data Primitif menjadi String.

---
+++ Days_020.java
@@ -0,0 +1,9 @@
+package Package;
+
+public class Days_020 {
+public static void main(String[] args) {
+    int angka = 12312321;
+    String konversi = String.valueOf(angka);
+    System.out.println(angka);
+}
+}
