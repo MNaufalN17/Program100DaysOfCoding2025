8c0cba17773d52721fac281e9893bcc991db05b5
Muhammad Naufal. N <146636243+MNaufalN17@users.noreply.github.com>
2025-10-02 00:37:11 +0800

    Operator Increment dan Decrement (++, --)
    Operator Increment dan Decrement (++, --)

---
+++ Days_025.java
@@ -0,0 +1,13 @@
+package Package;
+
+public class Days_025 {
+public static void main(String[] args) {
+    int angka1 = 9;
+    if (angka1==10) {
+        angka1++;
+    }else{
+        angka1--;
+    }
+    System.out.println(angka1);
+}
+}
