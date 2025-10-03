69b706bcd5482c7b5ab42c0835f9281d630f0df3
Muhammad Naufal. N <146636243+MNaufalN17@users.noreply.github.com>
2025-10-03 15:16:40 +0800

    Operator Penugasan (=, +=, -=, dll).
    Operator Penugasan (=, +=, -=, dll).

---
+++ Days_026.java
@@ -0,0 +1,20 @@
+package Package;
+
+public class Days_026 {
+public static void main(String[] args) {
+    int a = 50;
+    int b = 30;
+    int c = 25;
+
+    a*=b;
+    System.out.println(a);
+    a/=c;
+    System.out.println(a);
+    a-=b;
+    System.out.println(a);
+    a%=5;
+    System.out.println(a);
+
+    var angkaku = 90;
+}
+}
